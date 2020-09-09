4829736683ca4ba639f94a0772d9e551e8d1fdd1
SatishPatel8424 <satishkotadiya2013@gmail.com>
2020-09-09 18:03:29 +0530

    Product genric class based view

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from .views import products_view, product_detail_view, product_search
+from products import views
 
 urlpatterns = [
-    path("", products_view, name="products"),
-    path("detail/<int:primary_key>", product_detail_view, name="product_detail"),
-    path("search", product_search, name="product_search")
+
+    path('', views.products_view.as_view(), name='products'),
+
+    path('product/<int:pk>', views.product_detail_view.as_view(), name='product_detail'),
+    path('search', views.product_search.as_view(), name='product_search'),
+
+
 ]
