054816f284bbca7321433c0b9d2e97eedfb6a97b
SatishPatel8424 <satishkotadiya2013@gmail.com>
2020-09-14 12:31:44 +0530

    account genric class based view

--- accounts/views.py
+++ accounts/views.py
@@ -28,42 +28,52 @@ class registration(generic.FormView):
     form_class = UserRegisterForm
     template_name = 'register.html'
 
-    def post(self, request, *args, **kwargs):
-        if request.user.is_authenticated:
+    def get_form_kwargs(self):
+        """Return the keyword arguments for instantiating the form."""
+        kwargs = {
+            'initial': self.get_initial(),
+            'prefix': self.get_prefix(),
+        }
+        if self.request.method in ('POST', 'PUT'):
+            kwargs.update({
+                'data': self.request.POST,
+                'files': self.request.FILES,
+            })
+        return kwargs
+
+    def get_initial(self):
+        if self.request.user.is_authenticated:
             return redirect("profile")
-        previous_page = request.META.get("HTTP_REFERER")
+        previous_page = self.request.META.get("HTTP_REFERER")
         form = self.form_class(self.request.POST)
         if previous_page:
             if "next" in previous_page:
                 marker_index = previous_page.index("next")
                 string_length = len(previous_page)
                 redirect_string = previous_page[marker_index + 5:string_length]
-                request.session["redirect_target"] = redirect_string
-        if request.method == "POST":
-            if form.is_valid():
-                form.save()
-                sweetify.success(
-                    request,
-                    title="Account created successfully.",
-                    icon="success"
+                self.request.session["redirect_target"] = redirect_string
+        return self.initial.copy()
+
+    def form_valid(self, form):
+        form.save()
+        sweetify.success(
+            self.request,
+            title="Account created successfully.",
+            icon="success"
+        )
+        user = auth.authenticate(
+            self.request,
+            username=form.cleaned_data.get("username"),
+            password=form.cleaned_data.get("password1")
+        )
+        if user is not None:
+            auth.login(self.request, user)
+            if self.request.session.get("redirect_target"):
+                return HttpResponseRedirect(
+                    self.request.session["redirect_target"]
                 )
-                user = auth.authenticate(
-                    request,
-                    username=form.cleaned_data.get("username"),
-                    password=form.cleaned_data.get("password1")
-                )
-                if user is not None:
-                    auth.login(request, user)
-                    if request.session.get("redirect_target"):
-                        return HttpResponseRedirect(
-                            request.session["redirect_target"]
-                        )
-                    return redirect("profile")
-        else:
-            # form = UserRegisterForm()
-            form = self.form_class()
-            return JsonResponse({"success": True}, status=200)
-        return JsonResponse({"success": False, "errors": form.errors}, status=400)
+            return redirect("profile")
+        return HttpResponseRedirect(self.get_success_url())
 
 
 @method_decorator(login_required, name='dispatch')
@@ -71,33 +81,76 @@ class profile(generic.FormView):
     form_class = UserCredentialsUpdateForm
     template_name = 'profile.html'
 
-    def post(self, request, *args, **kwargs):
-        previous_page = request.META.get("HTTP_REFERER")
+
+    def get_form_kwargs(self):
+        """Return the keyword arguments for instantiating the form."""
+        kwargs = {
+            'initial': self.get_initial(),
+            'prefix': self.get_prefix(),
+        }
+        if self.request.method in ('POST', 'PUT'):
+            kwargs.update({
+                'data': self.request.POST,
+                'files': self.request.FILES,
+            })
+        return kwargs
+
+    def get_initial(self):
+        previous_page = self.request.META.get("HTTP_REFERER")
         if previous_page:
             if previous_page.endswith("login"):
                 sweetify.success(
-                    request,
+                    self.request,
                     title="Login successful.",
                     icon="success",
                 )
-        user = User.objects.get(username=request.user)
+        return self.initial.copy()
+
+    def form_valid(self, form):
+        user = User.objects.get(username=self.request.user)
         user_orders = OrderDetail.objects.filter(
             shipping__customer_id=user.id).order_by("-purchase_date")
-        if request.method == "POST":
-            form = UserCredentialsUpdateForm(request.POST, instance=user)
-            if form.is_valid():
-                new_credentials = form.save(commit=False)
-                hashed_password = make_password(form.cleaned_data["password"])
-                new_credentials.password = hashed_password
-                new_credentials.save()
-                sweetify.success(
-                    request,
-                    "Your credentials have been updated, please login."
-                )
-                return redirect("login")
-        else:
-            form = UserCredentialsUpdateForm(instance=user)
-            return JsonResponse({"success": True}, status=200)
-        return JsonResponse({"success": False, "errors": form.errors}, status=400)
+        if self.request.method == "POST":
+            form = UserCredentialsUpdateForm(self.request.POST, instance=user)
+            new_credentials = form.save(commit=False)
+            hashed_password = make_password(form.cleaned_data["password"])
+            new_credentials.password = hashed_password
+            new_credentials.save()
+            sweetify.success(
+                self.request,
+                "Your credentials have been updated, please login."
+            )
+            return redirect("login")
 
+        return HttpResponseRedirect(self.get_success_url())
+
+    # def post(self, request, *args, **kwargs):
+    #     previous_page = request.META.get("HTTP_REFERER")
+    #     if previous_page:
+    #         if previous_page.endswith("login"):
+    #             sweetify.success(
+    #                 request,
+    #                 title="Login successful.",
+    #                 icon="success",
+    #             )
+    #     user = User.objects.get(username=request.user)
+    #     user_orders = OrderDetail.objects.filter(
+    #         shipping__customer_id=user.id).order_by("-purchase_date")
+    #     if request.method == "POST":
+    #         form = UserCredentialsUpdateForm(request.POST, instance=user)
+    #         if form.is_valid():
+    #             new_credentials = form.save(commit=False)
+    #             hashed_password = make_password(form.cleaned_data["password"])
+    #             new_credentials.password = hashed_password
+    #             new_credentials.save()
+    #             sweetify.success(
+    #                 request,
+    #                 "Your credentials have been updated, please login."
+    #             )
+    #             return redirect("login")
+    #     else:
+    #         form = UserCredentialsUpdateForm(instance=user)
+    #         return JsonResponse({"success": True}, status=200)
+    #     return JsonResponse({"success": False, "errors": form.errors}, status=400)
+    #
 
